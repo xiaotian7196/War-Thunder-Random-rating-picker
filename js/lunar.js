23fc3cad8b4999306a2f4b9287373ec53f2660ea
gugugu625 <18210965650@163.com>
2024-10-11 16:01:56 +0800

    add specialevents

--- js/lunar.js
+++ js/lunar.js
@@ -5,25 +5,16 @@ function addEventsToColumn(data,right) {
     }
 
     data.events.forEach(event => {
-        // 创建 content-box 容器
         const contentBox = document.createElement('div');
         contentBox.className = 'content-box';
-
-        // 创建 content 元素
         const content = document.createElement('div');
         content.className = 'content';
         content.textContent = event.Title;
-
-        // 创建 description 元素
         const description = document.createElement('div');
         description.className = 'description';
         description.textContent = event.details;
-
-        // 将 content 和 description 添加到 content-box 中
         contentBox.appendChild(content);
         contentBox.appendChild(description);
-
-        // 将 content-box 添加到左侧栏中
         showColumn.appendChild(contentBox);
     });
 }
--- js/lunarContent.js
+++ js/lunarContent.js
@@ -1,4 +1,10 @@
 const data = {
+    specialevents:{
+        DoNothing: {
+            Title: "不宜战雷",
+            details: "玩点别的"
+        }
+    },
     events: [
         {
             Title: "炸战区",
