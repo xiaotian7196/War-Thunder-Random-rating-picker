835da29041e25ca7fd2758d9bb318bb5eef10cb4
XiaotianC <157281571+xiaotian7196@users.noreply.github.com>
2024-11-15 09:47:34 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//主表格
+
 const tabs = document.querySelectorAll(".tabs-container .tab");
 const contents = document.querySelectorAll(".tabs-container .content");
 
@@ -17,4 +19,12 @@ tabs.forEach((t, i) => {
     contents[i].classList.add("active");
     t.classList.add("active");
   });
+});
+
+
+//小窗口
+const notification = document.querySelector('.notification');
+const closeButton = document.querySelector('.notification .closeButton');
+closeButton.addEventListener('click', () => {
+  notification.style.display = 'none';
 });
