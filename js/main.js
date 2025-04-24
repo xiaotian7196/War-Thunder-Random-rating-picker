29b1c238f3b357e45859e696305756da73a4dadc
XiaotianC <157281571+xiaotian7196@users.noreply.github.com>
2025-04-24 14:53:28 +0800

    25.4.24 [NEW | DEL] 优化文件 | 修改界面排版 | 英文界面修改更新上一版中文的更新内容

--- js/main.js
+++ js/main.js
@@ -18,3 +18,14 @@ tabs.forEach((t, i) => {
         t.classList.add("active");
     });
 });
+
+function toggleImage(type) {
+    const checkbox = document.getElementById(`show${type}Image`);
+    const image = document.getElementById(type === 'Air' ? 'image1' :
+        type === 'Sea' ? 'image2' :
+            type === 'Plus' ? 'image3' : 'image');
+    const button = document.getElementById(`change${type}Button`);
+    const displayStyle = checkbox.checked ? 'block' : 'none';
+    image.style.display = displayStyle;
+    button.style.display = displayStyle;
+}
